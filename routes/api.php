173cc96ca1f6bc3e6a3438c2dbc169f81699b3c1
kurma <kurmangazy1994@gmail.com>
2024-07-19 02:55:13 +0500

    Refactor API routes to use apiResource for TaskController

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('tasks', [TaskController::class, 'index']);
-Route::post('tasks', [TaskController::class, 'store']);
-Route::get('tasks/show/{id}', [TaskController::class, 'show']);
-Route::put('tasks/{id}', [TaskController::class, 'update']);
-Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
-Route::get('tasks/search', [TaskController::class, 'search']);
+Route::apiResource('tasks', TaskController::class);
+
