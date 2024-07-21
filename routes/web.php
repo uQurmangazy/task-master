c95d0df6d8b800acd9acd0795671c2e9c4de87af
kurma <kurmangazy1994@gmail.com>
2024-07-21 20:30:16 +0500

    Code refactoring

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('');
-//});
-//
-//Route::apiResource('tasks', TaskController::class);
-//Route::get('tasks/search/{status}/{due_date}', [TaskController::class, 'search']);
+Route::get('/', function () {
+    return view('');
+});
